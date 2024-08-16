c167c6db9a13a2822f8302f2195bb740bf917737
isaac castro <okechukwuisaac111@gmail.com>
2024-08-16 12:24:21 +0100

    done with the composite documentation

---
+++ src/main/java/com/amazon/ata/horizonal/HorizonalApplication.java
@@ -0,0 +1,13 @@
+package com.amazon.ata.horizonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorizonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HorizonalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amazon/ata/horizonal/HorizonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amazon.ata.horizonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HorizonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
